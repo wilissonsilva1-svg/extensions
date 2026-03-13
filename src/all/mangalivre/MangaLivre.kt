0db3d7a2566e2c602b9b3855448e0974729e0200
wilissonsilva1-svg <wilissonsilva1@gmail.com>
2026-03-13 01:38:31 -0300

    Create MangaLivre.kt

---
+++ src/all/mangalivre/MangaLivre.kt
@@ -0,0 +1,9 @@
+package eu.kanade.tachiyomi.extension.all.mangalivre
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+
+class MangaLivre : Madara(
+    "MangaLivre",
+    "https://mangalivre.blog",
+    "pt-BR"
+)
